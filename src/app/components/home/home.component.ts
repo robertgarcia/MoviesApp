684d3a91d811e473cb87a598a82caddef31d0487
Roberto D. Garcia <robertdavid.mgt@gmail.com>
2019-08-26 10:50:25 -0600

    Diseño de la pantalla Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from '../../services/peliculas.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private service: PeliculasService
+  ) {
+    this.service.getPopulares().subscribe( res => console.log(res) );
+  }
 
   ngOnInit() {
   }
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -17,4 +17,15 @@ export class PeliculasService {
     const url = `${ this.urlMoviedb }/discover/movie?sort_by=popularity.desc&api_key${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
     return this.http.jsonp(url, 'JSONP_CALLBACK');
   }
+
+  getCartelera() {
+    const desde = new Date();
+    const hasta = new Date();
+    hasta.setDate(hasta.getDate() + 7);
+    const desdeStr = `${ desde.getFullYear() }-${ desde.getMonth() }-${ desde.getDay }`;
+    const hastaStr = `${ hasta.getFullYear() }-${ hasta.getMonth() }-${ hasta.getDay }`;
+    const url = `${ this.urlMoviedb }/discover/movie?primary_release_date.gte=${ desdeStr }-15&primary_release_date.lte=${ hastaStr }
+    &api_key${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+    return this.http.jsonp(url, 'JSONP_CALLBACK');
+  }
 }
